0ca13c8b5a2e6199730b32792f958cb8dfeeb85d
Hansrutger <bloody_pirate_4-ever@hotmail.com>
2016-04-12 01:34:40 +0200

    Project Created
    MS created project

---
+++ CorridorAPI/CorridorAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CorridorAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ CorridorAPI/CorridorAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CorridorAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
