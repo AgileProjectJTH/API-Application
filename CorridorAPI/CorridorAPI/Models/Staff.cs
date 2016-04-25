6c794c7200551480aea10b6f0f53c0ef87e77add
Björn <bf931@hotmail.com>
2016-04-25 13:52:23 +0200

    Added new class Staff

---
+++ CorridorAPI/CorridorAPI/Models/Staff.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CorridorAPI.Models
+{
+    public class Staff
+    {
+        public string signature { get; set; }
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public string mobile { get; set; }
+        public string mail { get; set; }
+        public List<Schedule> schedules { get; set; }
+    }
+}
