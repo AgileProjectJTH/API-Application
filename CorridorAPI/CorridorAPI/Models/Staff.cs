d1397645b4c004b7ccda85f156dd1725c005f00f
Björn <bf931@hotmail.com>
2016-04-25 15:10:35 +0200

    Moved the Json convertion from staffs to staff

--- CorridorAPI/CorridorAPI/Models/Staff.cs
+++ CorridorAPI/CorridorAPI/Models/Staff.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -7,6 +8,28 @@ namespace CorridorAPI.Models
 {
     public class Staff
     {
+        public Staff(JToken JsonStaff)
+        {
+            schedules = new List<Schedule>();
+            signature = (string)JsonStaff["Signature"];
+            firstname = (string)JsonStaff["Firstname"];
+            lastname = (string)JsonStaff["Lastname"];
+            mobile = (string)JsonStaff["Mobile"];
+            mail = (string)JsonStaff["Mail"];
+            JArray jScheArr = (JArray)JsonStaff["Schedule"];
+            for (int k = 0; k < jScheArr.Count; k++)
+            {
+                Schedule s = new Schedule();
+                s.room = (string)jScheArr[k]["Room"];
+                s.date = (string)jScheArr[k]["Date"];
+                s.from = (string)jScheArr[k]["From"];
+                s.to = (string)jScheArr[k]["To"];
+                s.signatures = (string)jScheArr[k]["Signatures"];
+                s.course = (string)jScheArr[k]["Course"];
+                s.moment = (string)jScheArr[k]["Moment"];
+                schedules.Add(s);
+            }
+        }
         public string signature { get; set; }
         public string firstname { get; set; }
         public string lastname { get; set; }
--- CorridorAPI/CorridorAPI/Models/Staffs.cs
+++ CorridorAPI/CorridorAPI/Models/Staffs.cs
@@ -19,27 +19,7 @@ namespace CorridorAPI.Models
             staffs = new List<Staff>();
             for (int i = 0; i < jStaffArr.Count; i++)
             {
-                Staff staff = new Staff();
-
-                staff.schedules = new List<Schedule>();
-                staff.signature = (string)jStaffArr[i]["Signature"];
-                staff.firstname = (string)jStaffArr[i]["Firstname"];
-                staff.lastname = (string)jStaffArr[i]["Lastname"];
-                staff.mobile = (string)jStaffArr[i]["Mobile"];
-                staff.mail = (string)jStaffArr[i]["Mail"];
-                JArray jScheArr = (JArray)jStaffArr[i]["Schedule"];
-                for (int k = 0; k < jScheArr.Count; k++)
-                {
-                    Schedule s = new Schedule();
-                    s.room = (string)jScheArr[k]["Room"];
-                    s.date = (string)jScheArr[k]["Date"];
-                    s.from = (string)jScheArr[k]["From"];
-                    s.to = (string)jScheArr[k]["To"];
-                    s.signatures = (string)jScheArr[k]["Signatures"];
-                    s.course = (string)jScheArr[k]["Course"];
-                    s.moment = (string)jScheArr[k]["Moment"];
-                    staff.schedules.Add(s);
-                }
+                Staff staff = new Staff(jStaffArr[i]);
                 staffs.Add(staff);
             }
         }
