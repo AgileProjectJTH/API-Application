32a280c2ba6be6b0ab208408e6287a2c20625d17
Björn <bf931@hotmail.com>
2016-04-25 13:45:00 +0200

    Created UserController with five empty functions

---
+++ CorridorAPI/CorridorAPI/Controllers/UserController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CorridorAPI.Controllers
+{
+    public class UserController : Controller
+    {
+        /* POST: /User
+           Creates a new user */
+        public ActionResult POST()
+        {
+            return null;
+        }
+        /* GET: /User
+           Returns: all users */
+        public ActionResult GET()
+        {
+            return null;
+        }
+        /* GET: /User
+           Returns: all users from a specific corridor */
+        public ActionResult GET(string corridorNr)
+        {
+            return null;
+        }
+        /* PUT: /User
+           Updates a user */
+        public ActionResult PUT()
+        {
+            return null;
+        }
+        /* DELETE: /User
+           Deletes a user */
+        public ActionResult DELETE()
+        {
+            return null;
+        }
+    }
+}
