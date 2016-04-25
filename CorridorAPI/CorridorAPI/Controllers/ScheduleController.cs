8a2b730b956d63089d029c5699397ae23ffefb5b
Björn <bf931@hotmail.com>
2016-04-25 14:54:40 +0200

    Added two controller with a few empty funktions

---
+++ CorridorAPI/CorridorAPI/Controllers/ScheduleController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace CorridorAPI.Controllers
+{
+    public class ScheduleController : ApiController
+    {
+        /* POST: Api/Schedule
+           Set Unavaible for current time span */
+        public ActionResult POST()
+        {
+            return null;
+        }
+        /* DELETE: Api/Schedule
+           Set avaiable for the current time span */
+        public ActionResult DELETE()
+        {
+            return null;
+        }
+    }
+}
--- CorridorAPI/CorridorAPI/Controllers/UserController.cs
+++ CorridorAPI/CorridorAPI/Controllers/UserController.cs
@@ -7,33 +7,33 @@ using System.Web.Mvc;
 
 namespace CorridorAPI.Controllers
 {
-    public class UserController : Controller
+    public class UserController : ApiController
     {
-        /* POST: /User
+        /* POST: Api/User
            Creates a new user */
         public ActionResult POST()
         {
             return null;
         }
-        /* GET: /User
+        /* GET: Api/User
            Returns: all users */
         public ActionResult GET()
         {
             return null;
         }
-        /* GET: /User
+        /* GET: Api/User
            Returns: all users from a specific corridor */
         public ActionResult GET(string corridorNr)
         {
             return null;
         }
-        /* PUT: /User
+        /* PUT: Api/User
            Updates a user */
         public ActionResult PUT()
         {
             return null;
         }
-        /* DELETE: /User
+        /* DELETE: Api/User
            Deletes a user */
         public ActionResult DELETE()
         {
