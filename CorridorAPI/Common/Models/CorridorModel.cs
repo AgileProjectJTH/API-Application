4b30e3d929d0b460527a7847152e6cb700349732
Björn <bf931@hotmail.com>
2016-05-06 20:56:18 +0200

    Implemented empty corridor funktions and corridorModel class

---
+++ CorridorAPI/Common/Models/CorridorModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Models
+{
+    public class CorridorModel
+    {
+        public int corridorId { get; set; }
+        public string corridorName { get; set; }
+    }
+}
--- CorridorAPI/CorridorAPI/Controllers/CorridorController.cs
+++ CorridorAPI/CorridorAPI/Controllers/CorridorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,74 @@ namespace CorridorAPI.Controllers
 {
     public class CorridorController : ApiController
     {
+        public IHttpActionResult POST(CorridorModel corridorModel)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        public IHttpActionResult POST(string username, string corridorId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        public IHttpActionResult DELETE(string corridorId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        public IHttpActionResult DELETE(string username, string corridorId)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        public IHttpActionResult PUT(CorridorModel corridorModel)
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+        public IHttpActionResult GET()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
     }
 }
