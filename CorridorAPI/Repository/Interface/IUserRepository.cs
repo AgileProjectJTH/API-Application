d21bad3a283558e5dc1f3ee0aa9f79fc95dbe6d7
Björn <bf931@hotmail.com>
2016-05-16 10:39:03 +0200

    Added Interface for UserRepository

---
+++ CorridorAPI/Repository/Interface/IUserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface IUserRepository
+    {
+        /// <summary>
+        /// Adds a new user
+        /// </summary>
+        /// <param name="user"></param>
+        void Post(Staff staff);
+    }
+}
