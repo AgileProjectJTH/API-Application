5e81d21088b844f26caab90f97a69ffd338eb852
Björn <bf931@hotmail.com>
2016-05-16 10:30:33 +0200

    Added Interface for ScheduleServices

--- CorridorAPI/Service/Interface/IScheduleServices.cs
+++ CorridorAPI/Service/Interface/IScheduleServices.cs
@@ -15,5 +15,19 @@ namespace Service.Interface
         /// <param name="roomNr">Number of staffs room</param>
         /// <returns>a list of Schedules</returns>
         List<Schedule> List(string roomNr);
+
+        /// <summary>
+        /// Adds a list of tasks
+        /// </summary>
+        /// <param name="lSchedule">Tasks to add</param>
+        /// <param name="username"></param>
+        void Post(List<Schedule> lSchedule, string username);
+
+        /// <summary>
+        /// Adds a Schedule
+        /// </summary>
+        /// <param name="schedule"></param>
+        /// <param name="username"></param>
+        void Post(Schedule schedule, string username);
     }
 }
