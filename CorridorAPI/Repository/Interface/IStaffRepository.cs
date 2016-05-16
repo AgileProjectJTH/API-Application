b2ed88c9a8d728d9a0ea90bad44944f5a8cc59b2
Björn <bf931@hotmail.com>
2016-05-16 10:38:11 +0200

    Added Interface for TaskRepository

--- CorridorAPI/Repository/Interface/IStaffRepository.cs
+++ CorridorAPI/Repository/Interface/IStaffRepository.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Interface
 {
-    interface IStaffRepository
+    public interface IStaffRepository
     {
         /// <summary>
         /// returns a staff with staffId = staffId
---
+++ CorridorAPI/Repository/Interface/ITaskRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Interface
+{
+    public interface ITaskRepository
+    {
+        /// <summary>
+        /// Returns a list of task for a specific staff with the roomNr = roomNr
+        /// </summary>
+        /// <param name="roomNr">Number of staffs room</param>
+        /// <returns>a list of Tasks</returns>
+        List<Task> List(string roomNr);
+
+        /// <summary>
+        /// Adds a list of tasks
+        /// </summary>
+        /// <param name="tasks">Tasks to add</param>
+        void Post(List<Task> tasks, string username);
+
+        /// <summary>
+        /// Adds a task
+        /// </summary>
+        /// <param name="task">task to add</param>
+        void Post(Task task, string username);
+    }
+}
