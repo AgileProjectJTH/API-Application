cbe49139984f88f9b1c99ae69256c09a356f5278
Björn <bf931@hotmail.com>
2016-05-16 10:32:24 +0200

    Added Interface for UserServices

---
+++ CorridorAPI/Service/Interface/IUserServices.cs
@@ -0,0 +1,18 @@
+﻿using Common.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Interface
+{
+    public interface IUserServices
+    {
+        /// <summary>
+        /// Adds a new user
+        /// </summary>
+        /// <param name="staff"></param>
+        void Post(StaffModel staff);
+    }
+}
