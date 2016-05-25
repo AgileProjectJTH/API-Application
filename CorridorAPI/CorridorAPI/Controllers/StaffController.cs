5b8382016e42432fc87e72d2a7cbfcbdec294b39
Björn <bf931@hotmail.com>
2016-05-25 16:36:13 +0200

    Added funktion /* GET: Api/Staff were Admin can get avaibility for all users

--- CorridorAPI/CorridorAPI/Controllers/StaffController.cs
+++ CorridorAPI/CorridorAPI/Controllers/StaffController.cs
@@ -15,13 +15,49 @@ namespace CorridorAPI.Controllers
 {
     public class StaffController : ApiController
     {
-
+        IStaffServices _staffServices;
         IScheduleServices _scheduleServices;
 
         public StaffController()
         {
             _scheduleServices = new ScheduleServices();
+            _staffServices = new StaffServices();
         }
+         
+        /* GET: Api/Staff
+        * Param: Date need format yyyy-mm-dd hh:mm:ss
+          Returns: Returns bool True if staff is available */
+        [Authorize]
+        public IHttpActionResult GET(string dateAndTime, string username)
+        {
+            var identity = User.Identity as ClaimsIdentity;
+            string authenticatedUser = identity.FindFirst("sub").Value;
+
+            DateTime dt;
+            if (!DateTime.TryParse(dateAndTime, out dt))
+            {
+                return BadRequest("wrong format on dateAndTime");
+            }
+
+            try
+            {
+                StaffModel user = _staffServices.Get(authenticatedUser);
+                if (user.isAdmin)
+                {
+                    return Ok(_scheduleServices.Get(dateAndTime, username));
+                }
+                else
+                {
+                    return BadRequest("Action not allowed for current user");
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        } 
 
         /* GET: Api/Staff
          * Param: Date need format yyyy-mm-dd hh:mm:ss
